4749fb3cda13c35269113e9939a91137015a2b04
lemon <fzh@.(none)>
2016-03-21 14:21:43 +0800

    common 3rd

--- a.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("hello world\n");
-	print("22222222222");
-	return 0;
-}
--- b.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	printf("the second use git \n");
-	return 0;
-}
